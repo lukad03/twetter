846069de14447bb33f1a703b47f9d1602d943c21
Luke Keller <lukeekeller@gmail.com>
2014-04-07 20:42:34 -0400

    create action for retwets

--- app/controllers/retwets_controller.rb
+++ app/controllers/retwets_controller.rb
@@ -3,13 +3,12 @@ class RetwetsController < ApplicationController
   before_filter :authenticate_user!
 
   def create
-    @twet = current_user.twets.create(twet_params)
-    if @twet.valid?
-      flash[:success] = "Your twet was shared"
+    retwet = current_user.retwets.create(retwet_params)
+    if retwet.valid?
+      flash[:success] = "You retwetted!"
       redirect_to user_profile_path(current_user.username)
     else
-      get_twets
-      flash[:error] = "Your twet could not be saved"
+      flash[:error] = "Your retwet could not be saved"
       render :action => :index and return
     end
   end
